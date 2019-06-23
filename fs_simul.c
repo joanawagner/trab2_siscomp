5b0a60eb5f72fe8a5fac1a14ac91103528c08df0
joanawagner <joana.wagner97@gmail.com>
2019-06-23 09:35:54 -0300

    Implementação de todos os comandos.

--- fs_simul.c
+++ fs_simul.c
@@ -7,7 +7,7 @@ void usage(char *exec){
 	printf("%s -format\n", exec);
 	printf("%s -create <disk file> <simulated file>\n", exec);
 	printf("%s -read <disk file> <simulated file>\n", exec);
-    printf("%s -ls <absolute directory path>\n", exec);
+    	printf("%s -ls <absolute directory path>\n", exec);
 	printf("%s -del <simulated file>\n", exec);
 	printf("%s -mkdir <absolute directory path>\n", exec);
 	printf("%s -rmdir <absolute directory path>\n", exec);
@@ -21,16 +21,41 @@ int main(int argc, char **argv){
 	}else{
 	
 		/* Disk formating. */
-		if( !strcmp(argv[1], "-format")){
+		if(!strcmp(argv[1], "-format")){
 			fs_format();
 		}
 		
+		//Criando arquivo
+		if(!strcmp(argv[1], "-create")){
+			fs_create(argv[2], argv[3]);
+		}
+
+		//Lê arquivo
+		if(!strcmp(argv[1], "-read")){
+			fs_read(argv[2], argv[3]);
+		}
+
+		//Mostra todos os arquivos do diretório de escolha
+		if(!strcmp(argv[1], "-ls")){
+			fs_ls(argv[2]);
+		}
+
+		//Deleta arquivo
+		if(!strcmp(argv[1], "-del")){
+			fs_del(argv[2]);
+		}
+		
+		//Criando diretório
+		if(!strcmp(argv[1], "-mkdir")){
+			fs_create(argv[2]);
+		}
+		
+		//Deleta diretório
+		if(!strcmp(argv[1], "-rmdir")){
+			fs_create(argv[2]);
+		}
+		
 	}
-	
-	
-	/* Implement the other filesystem calls here!! */
-	
-	
 	
 	/* Create a map of used/free disk sectors. */
 	fs_free_map("log.dat");
